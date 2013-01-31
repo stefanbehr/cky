99fd132351afce6ad1810f982a783503e1ea5c17
stefanbehr <sbehr1@gmail.com>
2013-01-30 18:38:01 -0800

    fixed condor command file

--- project1.cmd
+++ project1.cmd
@@ -2,10 +2,10 @@ Executable = project1.sh
 Universe   = vanilla
 getenv     = true
 input      = 
-output     = project1.out
+output     = output_data.out
 error      = project1.error
 Log        = project1.log
-arguments  = grammar.cfg grammar.cnf sentences.txt
+arguments  = grammar.cfg grammar.cnf data.txt
 transfer_executable = false
 request_memory = 4*1024
 Queue
